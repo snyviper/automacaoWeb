8cc09a2b23d4f760679feddcc3db5ecb70e8d9d2
Marcos Barbosa <marcos.costa@yaman.com.br>
2020-06-15 20:32:33 -0300

    Mayclon

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Selenium {
+
+@Test
+    public void criarUsuario(){
+    //Setar as propriedades do chrome Driver
+        System.setProperty("./src/main/resources/chromedriver","chromedriver");
+        //Criar instancia do driver do chrome
+        WebDriver driver = new ChromeDriver();
+        //Abrir browser e acessar a URL
+        driver.get("https://automacaocombatista.herokuapp.com/users/new");
+        //Identificando um elemento e preenchendo o campo com um texto
+        driver.findElement(By.xpath("user_name")).sendKeys("Marcos");
+        driver.findElement(By.id("user_lastname")).sendKeys("Barbosa");
+
+        //Fechando meu navagador
+//        driver.quit();
+    }
+
+
 }
