21509963c1c82e8f2fccfd48b446200b4c6b7bba
Marcos Barbosa <marcos.costa@yaman.com.br>
2020-06-15 21:51:51 -0300

    Subindo teste de criacao de usuario

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,25 +1,69 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Random;
 
 public class Selenium {
+    //Criar instancia do driver do chrome
+    private WebDriver driver = new ChromeDriver();
 
-@Test
-    public void criarUsuario(){
-    //Setar as propriedades do chrome Driver
+    @BeforeAll
+    public void setup(){
+        //Setar as propriedades do chrome Driver
         System.setProperty("./src/main/resources/chromedriver","chromedriver");
-        //Criar instancia do driver do chrome
-        WebDriver driver = new ChromeDriver();
         //Abrir browser e acessar a URL
-        driver.get("https://automacaocombatista.herokuapp.com/users/new");
-        //Identificando um elemento e preenchendo o campo com um texto
-        driver.findElement(By.xpath("user_name")).sendKeys("Marcos");
-        driver.findElement(By.id("user_lastname")).sendKeys("Barbosa");
+        driver.get("https://automacaocombatista.herokuapp.com");
+        //Maximizando o Browser Windows
+        //driver.manage().window().maximize();
 
-        //Fechando meu navagador
-//        driver.quit();
+        driver.findElement(By.xpath("//a[text()='Começar Automação Web']")).click();
+        //Maximizando o Browser Mac/Linux
+        driver.manage().window().fullscreen();
     }
 
+    @Test
+    public void criarUsuario(){
 
+        Random random = new Random();
+        String email = random.nextInt(10000)+"@bootcamp.com.br";
+
+        //Identificando um elemento e preenchendo o campo com um texto
+        driver.findElement(By.xpath("//a[text()='Formulário']")).click();
+        driver.findElement(By.xpath("//a[text()='Criar Usuários']")).click();
+        driver.findElement(By.id("user_name")).sendKeys("Marcos");
+        driver.findElement(By.id("user_lastname")).sendKeys("Barbosa");
+        driver.findElement(By.id("user_email")).sendKeys(email);
+        driver.findElement(By.id("user_address")).sendKeys("Avenida Trindade");
+        driver.findElement(By.id("user_university")).sendKeys("Nenhuma");
+        driver.findElement(By.id("user_profile")).sendKeys("Qa");
+        driver.findElement(By.id("user_gender")).sendKeys("M");
+        driver.findElement(By.id("user_age")).sendKeys("18");
+
+        //Clicando no botao Criar
+        driver.findElement(By.name("commit")).click();
+
+        //Atribuindo o texto da mensagem na tela para a variável
+        String mensagem = driver.findElement(By.xpath("//p[@id='notice']")).getText();
+
+        //Validando se a mensagem que eu necessito está sendo exibida na tela
+        Assertions.assertEquals("Usuário Criado com sucesso", mensagem);
+    }
+
+    @Test
+    public void radioCheckBox(){
+        driver.findElement(By.xpath("//a[text()='Busca de elementos']")).click();
+        driver.findElement(By.xpath("//a[text()='Radio e Checkbox']")).click();
+        
+
+    }
+
+    @AfterAll
+    public void fecharBrowser(){
+        //Fechando meu navagador
+        driver.quit();
+    }
 }
